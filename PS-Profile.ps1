97156d153387d364c86e4ad63235c17fedafb2f7
Ben Burge <blburge@afstores.com>
2022-03-15 12:38:46 -0600

    Updated Powershell Script

--- PS-Profile.ps1
+++ PS-Profile.ps1
@@ -1,12 +1,19 @@
 Clear-Host
 
-if(Get-Module -ListAvailable -Name Terminal-Icons)
+if(Get-Module -ListAvailable -Name posh-git)
 {
-    Import-Module -Name Terminal-Icons
 }
 else{
-    Install-Module -Name Terminal-Icons -Repository PSGallery
+    Install-Module -Name posh-git -Repository PSGallery -Scope CurrentUser
 }
 
+if(Get-Module -ListAvailable -Name Terminal-Icons)
+{
+}
+else{
+    Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser
+}
+
+Import-Module -Name posh-git
 Import-Module -Name Terminal-Icons
 oh-my-posh --init --shell pwsh --config https://raw.githubusercontent.com/BenBurge/DevelopmentTools/main/custom.omp.json | Invoke-Expression
